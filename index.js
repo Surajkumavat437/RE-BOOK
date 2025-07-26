43a5e00bf37ca9b40eba4fa58eb44b9a1c2a3b4d
surajkumavat437 <kumawatsuraj437@gmail.com>
2025-07-26 12:15:25 +0530

    Switch DB connection to MongoDB Atlas

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ const bookRoutes = require("./routes/bookRoutes");
 
 // Connect to DB
 mongoose
-  .connect("mongodb://127.0.0.1:27017/E-BOOKS")
+  .connect(process.env.DB_URL)
   .then(() => console.log("MongoDB connected"))
   .catch((err) => console.log(err));
 
